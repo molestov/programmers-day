b8e1931a3d176aad4a322126f36e1af66dcbbb38
molestov <alexander@molestov.ru>
2022-09-20 22:21:24 +0500

    Initial commit

---
+++ src/Praktikum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        // здесь нужно определить, является ли переданный год високосным
+        if (java.time.Year.of(year).isLeap()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
